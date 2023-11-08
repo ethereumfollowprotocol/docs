22ab6d5d24793a2d9bb89d3a87d0d7f0a5f16530
Omar <github@omaraziz.dev>
2023-11-07 21:04:52 -0800

    Add styles for action button and update background color for navigation bar

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ import starlightPlugin from '@astrojs/starlight-tailwind'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
   theme: {
     extend: {
       colors: {
