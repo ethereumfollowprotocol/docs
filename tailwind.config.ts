e3a12aa98168eb727a52233e69827b0a1e8c1208
Omar <github@omaraziz.dev>
2023-11-18 15:22:37 -0800

    update syntax highlighter

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,74 @@
 import colors from 'tailwindcss/colors'
 import type { Config } from 'tailwindcss'
+import typographyPlugin from '@tailwindcss/typography'
 import starlightPlugin from '@astrojs/starlight-tailwind'
+
+const linkHeadingStyles = {
+  color: colors.gray[100],
+  borderBottomColor: 'transparent',
+  '&:hover': {
+    color: `${colors.gray[900]}`
+  }
+}
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   darkMode: 'class',
+  important: true,
   theme: {
     extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            pre: {
+              background: 'rgba(205, 200, 255, 0.05)'
+            },
+            'h2 a': linkHeadingStyles,
+            'h3 a': linkHeadingStyles,
+            'h4 a': linkHeadingStyles,
+            'h5 a': linkHeadingStyles,
+            'h6 a': linkHeadingStyles,
+            blockquote: {
+              fontSize: '90%',
+              color: colors.zinc[500],
+              borderLeftColor: colors.zinc[700],
+              'p::before': { display: 'none' },
+              'p::after': { display: 'none' }
+            },
+            a: {
+              textDecoration: 'none',
+              borderBottom: `2px solid ${colors.cyan[800]}`,
+              color: colors.cyan[400],
+              transition: 'color 0.2s ease, border-color 0.2s ease, background 0.2s ease',
+              '&:hover': {
+                color: `${colors.zinc[900]} !important`,
+                borderBottomColor: `${colors.cyan[200]} !important`,
+                background: colors.cyan[200]
+              }
+            },
+            code: {
+              color: '#86e1fc',
+              '&::before': { content: `unset !important` },
+              '&::after': { content: `unset !important` },
+              fontWeight: 'normal'
+            },
+            '[data-rehype-pretty-code-fragment]:nth-of-type(2) pre': {
+              '[data-line]::before': {
+                content: 'counter(line)',
+                counterIncrement: 'line',
+                display: 'inline-block',
+                width: '1rem',
+                marginRight: '1rem',
+                textAlign: 'right',
+                color: colors.slate[600]
+              },
+              '[data-highlighted-line]::before': {
+                color: colors.slate[400]
+              }
+            }
+          }
+        }
+      },
       colors: {
         accent: {
           '50': '#fdf2f8',
@@ -26,5 +88,5 @@ export default {
       }
     }
   },
-  plugins: [starlightPlugin()]
+  plugins: [starlightPlugin(), typographyPlugin]
 } satisfies Config
