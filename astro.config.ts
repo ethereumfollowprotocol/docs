950acedccfb073cc4d3ad391e9cbb756b9d98efd
0xthrpw <0xthrpw@gmail.com>
2025-04-10 21:45:26 -0400

    bulk subs

--- astro.config.ts
+++ astro.config.ts
@@ -129,7 +129,7 @@ export default defineConfig({
               link: '/production/silo'
             },
             {
-                label: 'EFP Follow Bot',
+                label: 'Telegram Bot',
                 link: '/production/follow-bot'
             },
             {
